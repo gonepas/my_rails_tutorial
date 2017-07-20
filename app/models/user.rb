4fc26e20984148ee4a1d6cabc3866c8dbe0617c3
Do Xuan Tho <gonepas08@gmail.com>
2017-07-20 15:00:08 +0700

    chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
+  attr_reader :remember_token
   has_secure_password
 
   validates :email, format: {with: VALID_EMAIL_REGEX},
@@ -22,6 +22,24 @@ class User < ApplicationRecord
         end
       BCrypt::Password.create string, cost: cost
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    @remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.blank?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
   end
 
   private
