3757214afe516de8b65e319ca0e71c18369e0c95
Do Xuan Tho <gonepas08@gmail.com>
2017-07-21 11:16:57 +0700

    showing update delete users

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem "will_paginate", "3.1.5"
+gem "bootstrap-will_paginate", "1.0.0"
+gem "faker", "1.8.4"
 gem "config"
 gem "bcrypt", "3.1.11"
 gem "rails-controller-testing"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(new create show)
+  before_action :find_user, except: %i(index new create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
   attr_reader :user
 
-  def show
-    @user = User.find_by id: params[:id]
-    return if user
-    flash[:danger] = t "user not found"
-    redirect_to root_path
+  def index
+    @users = User.paginate page: params[:page]
   end
 
   def new
@@ -24,10 +26,52 @@ class UsersController < ApplicationController
     end
   end
 
+  def show; end
+
+  def edit; end
+
+  def update
+    if user.update_attributes user_params
+      flash[:success] = t "user.updated"
+      redirect_to user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    user.destroy
+    flash[:success] = t "user.delete"
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "user.please_log_in"
+    redirect_to login_url
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+
+    return if user
+    flash[:danger] = t "user.not_exist"
+    redirect_to root_path
+  end
+
+  def correct_user
+    redirect_to root_url unless @user.current_user? current_user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
